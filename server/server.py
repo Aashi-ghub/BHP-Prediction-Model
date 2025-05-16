e4afdfc2d01c65863bc2f9f7ae05ed6c71a6e37f
Aashi-ghub <161113739+Aashi-ghub@users.noreply.github.com>
2025-05-16 12:08:22 +0530

    Add home route to provide welcome message for the API

--- server/server.py
+++ server/server.py
@@ -3,6 +3,10 @@ import util
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "Welcome to Home Price Prediction API"
+
 @app.route('/get_location_names', methods=['GET'])
 def get_location_names():
     response = jsonify({
